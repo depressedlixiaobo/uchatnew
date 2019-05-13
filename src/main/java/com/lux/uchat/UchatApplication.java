2c6b2a6d3ce6aa23def546ac07537aa09b3ceb57
libo <398389303@qq.com>
2019-05-13 15:22:45 +0800

    first init

---
+++ src/main/java/com/lux/uchat/UchatApplication.java
@@ -0,0 +1,13 @@
+package com.lux.uchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UchatApplication.class, args);
+    }
+
+}
